8a683f007991d5a8a1391a721eb8cccf4b76e437
regisx001 <zarqi.ezzoubair@etu.uae.ac.ma>
2025-05-09 22:49:41 +0100

    feat(chat-button): replace div with p-button for chat toggle and update imports

--- src/app/components/chatbot/chat-button/chat-button.component.ts
+++ src/app/components/chatbot/chat-button/chat-button.component.ts
@@ -1,8 +1,9 @@
 import { Component, inject } from '@angular/core';
 import { ChatService } from '../../../services/chat.service';
-
+import { ButtonModule } from 'primeng/button';
 @Component({
   selector: 'app-chat-button',
+  imports: [ButtonModule],
   templateUrl: './chat-button.component.html',
 })
 export class ChatButtonComponent {
